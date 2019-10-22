fb81341758980b6fa9580fc285c9c4e55159b855
Sandra <grahlsan@gmail.com>
2019-10-22 17:28:22 +0100

    cleaned up header and made smoother loading

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,18 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Image from "../components/image"
+import styled, { keyframes } from "styled-components"
 
+const fadeInRight = keyframes`
+    from {
+      opacity: 0;
+      transform: translateX(-15px);
+    }
+    to {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  `
 
 const element = {
   gridTemplateColumns: `repeat(4, 1fr)`,
@@ -10,11 +21,46 @@ const element = {
   fontFamily: `Helvetica, Roboto, 'Open Sans'`,
 }
 
+const Welcome = styled.h2`
+letter-spacing: 1px;
+font-family: 'Archivo Black', Impact; 
+color: #333333;
+animation: ${fadeInRight} ease 1.2s forwards;
+`
+
+const MainHeading = styled.h1`
+color: #333333;
+text-decoration: none;
+text-align: center;
+font-family: 'Archivo Black'; Impact;
+font-weight: bolder;
+z-index: 1;
+line-height: 1.5;
+font-size: 2.4rem;
+margin-top: 4rem;
+margin-bottom: -1rem;
+letter-spacing: 1px;
+animation: ${fadeInRight} ease 1.4s forwards;
+`
+
+const ImageWrapper = styled.div`
+max-width: 500px;
+max-height: 500px;
+z-index: 1;
+animation: ${fadeInRight} ease 1.6s forwards;
+`
+
+const Quote = styled.p`
+letter-spacing: 2px;
+line-height: 1.8;
+animation: ${fadeInRight} ease 1.4s forwards;
+`
+
 const Header = ({ siteTitle }) => (
   <header>
   <section
     style={{
-      // backgroundSize: `cover`,
+      // backgroundSize: `cover`;
       padding: `4rem 2rem`,
       display: `grid`,
       alignItems: `center`,
@@ -23,34 +69,19 @@ const Header = ({ siteTitle }) => (
       marginBottom: `6rem`,
   }}>
     <div style={element}>
-    <h2 style={{fontFamily: `'Archivo Black', Impact`, color: `#333333`, transform: `translateY(-90px)`, letterSpacing: `1px`,}}>Welcome to</h2>
-        <h1
-          style={{
-            color: `#333333`,
-            textDecoration: `none`,
-            textAlign: `center`,
-            fontFamily: `'Archivo Black', Impact`,
-            fontWeight: `bolder`,
-            zIndex: `1`,
-            transform: `translate(2px, -90px)`,
-            lineHeight: `1.5`,
-            fontSize: `200%`,
-            marginTop: `3.8rem`,
-            marginBottom: `-2.4rem`,
-            letterSpacing: `1px`,
-          }}
-        > 
+    <Welcome style={{marginTop: `-6rem`}}>Welcome to</Welcome>
+        <MainHeading> 
           {siteTitle}
-        </h1>
+        </MainHeading>
       </div>
       <div style={element}>
-      <div style={{ maxWidth: `500px`, maxHeight: `500px`, zIndex: `1`}}>
+      <ImageWrapper>
       <Image />
-      </div>
+      </ImageWrapper>
     </div>
     <article style={element}>
-<p style={{letterSpacing: `2px`, lineHeight: `2`, transform: `translateY(70px)`,}}>“I do not wish women to have power over men but over <b>themselves</b>” <br />
-<i>A Vindication of the Rights of Woman (1792)</i></p>
+<Quote>“I do not wish women to have power over men but over <b>themselves</b>” <br />
+<i>A Vindication of the Rights of Woman (1792)</i></Quote>
     </article>
 
     </section>
