3191d9ea70bd504a5fc172117a7981456c1bb52d
Sandra <grahlsan@gmail.com>
2019-11-07 11:38:17 +0000

    imported typefaces instead of preloading

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,3 @@
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
@@ -8,14 +7,15 @@ import "./layout.css"
 import Footer from "./footer"
 import { Helmet } from "react-helmet"
 import "typeface-archivo-black"
-import { createGlobalStyle } from 'styled-components'
+import "typeface-roboto"
+import "typeface-yeseva-one"
+import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Archivo Black', Impact, sans-serif;
   }
 `
-
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -44,12 +44,15 @@ const Layout = ({ children }) => {
           },
         ]}
       >
-        <html lang="en" style={{fontFamily: `'Archivo Black', Impact, sans-serif`}}/>
+        <html
+          lang="en"
+          style={{ fontFamily: `'Archivo Black', Impact, sans-serif` }}
+        />
         <link rel="canonical" href="https://wollstonecraftwalks.netlify.com" />
-        <link rel="preload" href="https://wollstonecraftwalks.netlify.com/static/yeseva-one-latin-400-7a862e29ede310b5ba103d2a43e1b441.woff2" as="font" />
-        <link rel="preload" href="https://wollstonecraftwalks.netlify.com/static/roboto-latin-400-479970ffb74f2117317f9d24d9e317fe.woff2" as="font" />
-
-<meta name="google-site-verification" content="ZtGNrohmz3XFRtLU30FDSYpxw4d5jQe70PdolyjkVes" />
+        <meta
+          name="google-site-verification"
+          content="ZtGNrohmz3XFRtLU30FDSYpxw4d5jQe70PdolyjkVes"
+        />
       </Helmet>
       <GlobalStyle />
       <Header siteTitle={data.site.siteMetadata.title} />
