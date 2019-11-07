fab4649635c75ad94268f6c40676c4f2d70c2922
Sandra <grahlsan@gmail.com>
2019-11-07 11:05:23 +0000

    changes to helmet after running seo crawler

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -7,7 +7,6 @@ import Menu from "../components/menu"
 import BackButton from "../components/backButton"
 import "typeface-archivo-black"
 import 'typeface-yeseva-one'
-import { Helmet } from "react-helmet"
 
 class IntroPage extends React.Component {
     constructor() {
@@ -21,25 +20,6 @@ class IntroPage extends React.Component {
   render() {
     return (
         <> 
-              <Helmet
-        title={data.site.siteMetadata.title}
-        meta={[
-          {
-            name: "description",
-            content: "Walk guide app about Mary Wollstonecraft",
-          },
-          {
-            name: "keywords",
-            content:
-              "marywollstonecraft, wollstonecraft, walkguide, historiclondon, newingtongreen, feminism, britishfeminists, herstory, londonwalks, britishhistory, fragments&monuments",
-          },
-        ]}
-      >
-        <html lang="en" style={{fontFamily: `'Archivo Black', Impact, sans-serif`}}/>
-        <link rel="canonical" href="https://wollstonecraftwalks.netlify.com" />
-
-<meta name="google-site-verification" content="ZtGNrohmz3XFRtLU30FDSYpxw4d5jQe70PdolyjkVes" />
-      </Helmet>
       <GlobalStyle />
       <div style={{display: `grid`, justifyContent: `center`, alignItems: `center`, textAlign: `center`,}}>
         <div
